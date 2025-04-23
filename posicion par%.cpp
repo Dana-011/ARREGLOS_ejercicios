4b4d744bbb6c6e1b78f3f5ba61d7cf1fcd320909
Dana-011 <df2024016556@virtual.upt.pe>
2025-04-23 12:20:50 -0500

    Add files via upload

---
+++ posicion par%.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main(){
+	setlocale(LC_ALL, "SPANISH");
+	int A[10],n;
+	cout<<"ingrese la cantidad de números: ";
+	cin>>n;
+	for (int i=1;i<=n;i++){
+		cout<<"ingrese los elementos del vector: ";
+		cin>>A[i];
+	}
+	
+	cout<<"Los valores almacenados en las posiciones pares son : ";
+	for (int i=2;i<=n;i++){
+		 if (i%2==0){
+		  cout<<A[i]<<endl;
+		  }
+	}
+	return 0;
+}
+
